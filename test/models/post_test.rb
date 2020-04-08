1a6bdb9ff717d4cc577d82553ef75d0db10d123f
Carlos Anriquez <canriquez@gmail.com>
2020-04-08 15:07:40 -0300

    added tests for models

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(username: 'Example user', email: 'user@example.com')
+    @user.save
+    @post = Post.new(title: 'Example user', body: 'this is the generic body', 
+    user: @user)
+  end
+
+  test 'should be valid' do
+    assert @post.valid?
+  end
+
+  test 'title should be the right length' do
+    @post.title = 'a' * 300
+    assert_not @post.valid?
+  end
+
+  test 'body should be the right length' do
+    @post.body = 'aa'
+    assert_not @post.valid?
+  end
+
+
 end
